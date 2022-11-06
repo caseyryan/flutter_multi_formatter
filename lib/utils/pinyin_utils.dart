ef8914ead2d0b2c2dba11b1b84b56becc565b797
Konstantin Serov <konstantin.p.serov@gmail.com>
2022-11-06 23:13:30 +0700

    formattings of code

--- lib/utils/pinyin_utils.dart
+++ lib/utils/pinyin_utils.dart
@@ -775,7 +775,8 @@ class PinyinUtils {
         removePunctuation: removePunctuation,
       );
     } else {
-      final maxScoredSentences = _getSentencesWithMaxScore(allPossibleSentences!);
+      final maxScoredSentences =
+          _getSentencesWithMaxScore(allPossibleSentences!);
       allPossibleSentences.clear();
       allPossibleSentences.addAll(maxScoredSentences);
     }
@@ -985,7 +986,8 @@ class _Sentence {
     }
     if (rankByUse) {
       for (var r in _correctSequence!) {
-        List<HanziRankInfo> rankInfo = HanziUtils.findHanziRankByPinyin(r.value);
+        List<HanziRankInfo> rankInfo =
+            HanziUtils.findHanziRankByPinyin(r.value);
         int rank = rankInfo.firstOrNull?.fequencyRank ?? 10000;
         _totalUseRank += rank;
       }
